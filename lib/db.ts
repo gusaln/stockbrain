021cc091583aa3e704d0aaf3ec3d72780ca6d8cc
Gustavo López <git.gustavolopez.xyz@gmail.com>
2024-02-25 17:36:01 -0400

    feat: Crea conexión a la base de datos

--- lib/db.ts
+++ lib/db.ts
@@ -1,3 +1,15 @@
+import mysql from 'mysql'
 
-
-export const connection = null
+/**
+* connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+*     if (error) throw error;
+*     console.log('The solution is: ', results[0].solution);
+* });
+*/
+export const connection = mysql.createPool({
+    connectionLimit: 10,
+    host: process.env.DATABASE_HOST || '127.0.0.1',
+    user: process.env.DATABASE_USER || 'stockbrain',
+    password: process.env.DATABASE_SECRET || 'secret',
+    database: process.env.DATABASE_NAME || 'stockbrain',
+});
